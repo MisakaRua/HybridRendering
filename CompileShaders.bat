5154b87fada81358a5b6685870b6692858829be0
MisakaRua <1150235785@qq.com>
2022-07-09 19:01:41 +0800

    dxup

--- CompileShaders.bat
+++ CompileShaders.bat
@@ -1,3 +1,7 @@
 .\tools\shaderc.exe -f .\hybrid_rendering\shaders\src\cubes\vs_cubes.sc -i .\hybrid_rendering\shaders\include\ -o .\hybrid_rendering\shaders\bin\dx11\vs_cubes.bin --type vertex -p vs_5_0 --varyingdef .\hybrid_rendering\shaders\src\cubes\varying.def.sc
 
 .\tools\shaderc.exe -f .\hybrid_rendering\shaders\src\cubes\fs_cubes.sc -i .\hybrid_rendering\shaders\include\ -o .\hybrid_rendering\shaders\bin\dx11\fs_cubes.bin --type fragment -p ps_5_0 --varyingdef .\hybrid_rendering\shaders\src\cubes\varying.def.sc
+
+.\tools\shaderc.exe -f .\hybrid_rendering\shaders\src\cubes\vs_cubes.sc -i .\hybrid_rendering\shaders\include\ -o .\hybrid_rendering\shaders\bin\dx9\vs_cubes.bin --type vertex -p vs_3_0 --varyingdef .\hybrid_rendering\shaders\src\cubes\varying.def.sc
+
+.\tools\shaderc.exe -f .\hybrid_rendering\shaders\src\cubes\fs_cubes.sc -i .\hybrid_rendering\shaders\include\ -o .\hybrid_rendering\shaders\bin\dx9\fs_cubes.bin --type fragment -p ps_3_0 --varyingdef .\hybrid_rendering\shaders\src\cubes\varying.def.sc
--- SolutionConfiguration.bat
+++ SolutionConfiguration.bat
@@ -1,3 +1,2 @@
-cmake -B build -G "Visual Studio 15 2017" -A x64 
+cmake -B build -G "Visual Studio 15 2017" -A x64
 .\CompileShaders.bat
-Copy-Item -Path .\hybrid_rendering\shaders\bin\dx11 -Destination .\build\shaders\ -Recurse 
